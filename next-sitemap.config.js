8f189f5fb817bb13c1f67abb6f392fd00c1301e3
Jamil Gonzales <yajamil@thereadyconsult.com>
2026-04-01 21:51:42 -0500

    chore(01-02): remove next-sitemap package, config, and build script
    - Uninstalled next-sitemap@4.2.3 from dependencies
    - Deleted next-sitemap.config.js from project root
    - Changed build script from "next build && next-sitemap --config next-sitemap.config.js" to "next build"
    - Resolves CRIT-02: single sitemap system, no competing out/sitemap.xml

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://www.ginagonzaleznotary.com",
-  generateRobotsTxt: false,
-  trailingSlash: true,
-  outDir: "./out",
-};
